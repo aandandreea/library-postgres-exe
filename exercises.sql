dc0cfc79ef465cf4a24f31415c78f1b93b098639
aandandreea <126575169+aandandreea@users.noreply.github.com>
2025-06-01 13:29:05 +0300

    Update exercises.sql

--- exercises.sql
+++ exercises.sql
@@ -3,5 +3,9 @@ select title,name
 from books
 inner join authors
 on books.id=authors.id;
+--Find all members who joined in the year 2024.
+select join_date,name
+from members
+where extract(year from join_date)=2024;
 
 
